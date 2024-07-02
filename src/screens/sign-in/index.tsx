e250caafd2210f7feed655104a951003adc2f2e7
gildembergleite <gildembergleite@gmail.com>
2024-07-02 14:39:25 -0300

    refactor: add new components in sing in screen

--- src/screens/sign-in/index.tsx
+++ src/screens/sign-in/index.tsx
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { Button } from '@/components/button'
+import theme from '@/theme'
+import { ImageBackground, Text } from 'react-native'
 import { styles } from './styles'
 
 export function SignIn() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={theme.BACKGROUND_IMAGE} style={styles.view}>
+      <Text style={styles.title}>Car Fleet</Text>
+      <Text style={styles.slogan}>Gestão de uso de veículos</Text>
+      <Button title="Entrar com Google" />
+    </ImageBackground>
   )
 }
--- src/screens/sign-in/styles.ts
+++ src/screens/sign-in/styles.ts
@@ -1,10 +1,26 @@
 import { StyleSheet } from 'react-native'
+import theme from '../../theme'
 
 export const styles = StyleSheet.create({
-  container: {
+  view: {
+    display: 'flex',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 52,
+    backgroundColor: theme.COLORS.GRAY_800,
+  },
+  title: {
+    color: theme.COLORS.BRAND_LIGHT,
+    fontSize: theme.FONT_SIZE.XXXL,
+    fontFamily: theme.FONT_FAMILY.BOLD,
+    textAlign: 'center',
+  },
+  slogan: {
+    color: theme.COLORS.GRAY_100,
+    fontSize: theme.FONT_SIZE.MD,
+    fontFamily: theme.FONT_FAMILY.REGULAR,
+    textAlign: 'center',
+    marginBottom: 32,
   },
 })
