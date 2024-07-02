15e9b6fd489eb65d905c00f934e2a49dd0a6514d
gildembergleite <gildembergleite@gmail.com>
2024-07-02 14:37:43 -0300

    feat: loaded font and add loading state

--- App.tsx
+++ App.tsx
@@ -1,5 +1,37 @@
+/* eslint-disable camelcase */
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+import { StatusBar } from 'react-native'
+import { Loading } from './src/screens/loading'
 import { SignIn } from './src/screens/sign-in'
 
 export default function App() {
-  return <SignIn />
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  if (!fontsLoaded) {
+    return (
+      <>
+        <StatusBar
+          barStyle={'light-content'}
+          backgroundColor={'transparent'}
+          translucent
+        />
+        <Loading />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={'transparent'}
+        translucent
+      />
+      <SignIn />
+    </>
+  )
 }
